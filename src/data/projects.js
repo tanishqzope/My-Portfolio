997d7d0645be60e6886851915371b5bbcf4b33be
Tanishq  Atul Zope <tanishqzope5@gmail.com>
2026-06-09 23:13:07 +0530

    Update project ID and add new project details

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,6 @@
 export const projects = [
   {
-    id: 1,
+    id: 2,
     title: "-----",
     description: "------",
     image: "----",
@@ -8,6 +8,17 @@ export const projects = [
     githubUrl: "#",
     liveUrl: "#",
     status: "-----",
+    featured: true
+  },
+   {
+    id: 1,
+    title: "TDE Tool - Tanishq`s Decoder & Encoder",
+    description: "A fast, zero-dependency CLI tool for Base64 encoding and decoding. Built with pure Python — works on Windows, Linux, and macOS.",
+    image: "https://res.cloudinary.com/dx1zgcm3j/image/upload/v1781026818/TDE_2_pmdhpj.png",
+    techStack: ["Python", "Cli", "Base64", "Cross-Platfrom", "Devtools", "Encoder", "Decoder", "CyberSecurity", "Command-Line-Tool", "Url-Safe"],
+    githubUrl: "https://github.com/tanishqzope/TDE-Tool",
+    liveUrl: "#",
+    status: "Live",
     featured: true
   },
 ];
