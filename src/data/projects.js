5135318d4d71489a0ef4461dd6d0d253a65d5954
Tanishq  Atul Zope <tanishqzope5@gmail.com>
2026-06-09 23:16:44 +0530

    Update projects list and live URL for TDE Tool
    Removed a placeholder project and updated the live URL for the TDE Tool project.

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,15 +1,4 @@
 export const projects = [
-  {
-    id: 2,
-    title: "-----",
-    description: "------",
-    image: "----",
-    techStack: ["-----", "-----", "-----", "-----"],
-    githubUrl: "#",
-    liveUrl: "#",
-    status: "-----",
-    featured: true
-  },
    {
     id: 1,
     title: "TDE Tool - Tanishq`s Decoder & Encoder",
@@ -17,8 +6,10 @@ export const projects = [
     image: "https://res.cloudinary.com/dx1zgcm3j/image/upload/v1781026818/TDE_2_pmdhpj.png",
     techStack: ["Python", "Cli", "Base64", "Cross-Platfrom", "Devtools", "Encoder", "Decoder", "CyberSecurity", "Command-Line-Tool", "Url-Safe"],
     githubUrl: "https://github.com/tanishqzope/TDE-Tool",
-    liveUrl: "#",
+    liveUrl: "https://github.com/tanishqzope/TDE-Tool",
     status: "Live",
     featured: true
   },
 ];
+
+
